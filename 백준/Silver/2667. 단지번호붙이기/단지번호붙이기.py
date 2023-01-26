05ee1488900eded6a97f4ce00a4b50f2fa003061
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-01-26 15:41:35 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 30628 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,47 @@
+N = int(input())
+home_map = []
+for i in range(N):
+    home_map.append(list(map(int,input())))
+
+def check(y,x):
+    result = 0
+    if home_map[y][x] == 1:
+        home_map[y][x] = 2
+        result += 1
+    try:
+        if y > 0 and home_map[y-1][x] == 1:
+            home_map[y-1][x] += 1
+            result += check(y-1,x) + 1
+    except:
+        pass
+    try:
+        if home_map[y+1][x] == 1:
+            home_map[y+1][x] += 1
+            result += check(y+1,x) + 1
+    except:
+        pass
+    try:
+        if x > 0 and home_map[y][x-1] == 1:
+            home_map[y][x-1] += 1
+            result += check(y,x-1) + 1
+    except:
+        pass
+    try:
+        if home_map[y][x+1] == 1:
+            home_map[y][x+1] += 1
+            result += check(y,x+1) + 1
+    except:
+        pass
+    return result
+
+count = list()
+for y in range(N):
+    for x in range(N):
+        if home_map[y][x] == 1:
+            size = check(y,x)
+            count.append(size)
+
+count.sort()
+print(len(count))
+for i in count:
+    print(i)

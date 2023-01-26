d73f09dfbe0418a44f3771bdfd9156e63b2dd4bf
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-01-26 16:42:08 +0900

    [Bronze II] Title: 분산처리, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,10 +1,9 @@
 T = int(input())
-for test_case in range(1,T+1):
+for t in range(1,T+1):
     a, b = map(int,input().split())
     a = a % 10
-
-    if a > 0:
-        if b % 4 > 0:
+    if a:
+        if b % 4:
             print(a ** (b%4) % 10)
         else:
             print(a ** 4 % 10)

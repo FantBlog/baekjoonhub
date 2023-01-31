c18982fcd365b59fc6c1465a36bd7eced1bf3288
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-01-31 18:44:20 +0900

    [Bronze II] Title: 방 배정, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -1,16 +1,14 @@
 import sys
-N, K = map(int,sys.stdin.readline().rstrip('\n').split())
+input = sys.stdin.readline
+n,k = map(int,input().split())
 room_list = list()
-for i in range(6):
+for _ in range(6):
     room_list.append([0,0])
-
-for i in range(N):
-    gender, grade = map(int,sys.stdin.readline().rstrip('\n').split())
+for _ in range(n):
+    gender, grade = map(int,input().split())
     room_list[grade-1][gender] += 1
-
 count = 0
 for i in range(6):
     for j in range(2):
-        count += room_list[i][j] // K
-        count += room_list[i][j] % K
+        count += room_list[i][j] // k + room_list[i][j] % k
 print(count)

3c47c758d6ef12f5cece2bbdc2bd8db0102fb239
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-01-31 11:54:23 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -5,28 +5,20 @@ for n in range(N):
     order = sys.stdin.readline().rstrip('\n')
     if order[:3] == 'pus':
         stack.append(int(order.split()[1]))
-        continue
     elif order[:3] == 'top':
         try:
             print(stack[-1])
-            continue
         except:
             print('-1')
-            continue
     elif order[:3] == 'siz':
         print(len(stack))
-        continue
     elif order[:3] == 'emp':
         if len(stack):
             print('0')
-            continue
         else:
             print('1')
-            continue
     elif order[:3] == 'pop':
         try:
             print(stack.pop())
-            continue
         except:
             print('-1')
-            continue

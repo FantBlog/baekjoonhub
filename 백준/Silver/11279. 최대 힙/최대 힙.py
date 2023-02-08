d3ce5c6f7a1d4310911c82c3cebeee77ac589a7c
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-02-09 00:53:11 +0900

    [Silver II] Title: 최대 힙, Time: 936 ms, Memory: 118400 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,55 @@
+import sys
+input = sys.stdin.readline
+
+def in_hip(hip,num):
+    hip.append(num)
+    mom_idx = (len(hip)-1) // 2
+    son_idx = (len(hip)-1)
+    while son_idx >= 2:
+        if hip[mom_idx] < hip [son_idx]:
+            hip[mom_idx] , hip [son_idx] = hip[son_idx] , hip [mom_idx]
+        else:
+            break
+        son_idx = mom_idx
+        mom_idx //= 2
+    return hip
+
+def out_hip(hip):
+    if len(hip) == 1:
+        print(0)
+        return hip
+    
+    print(hip.pop(1))
+
+    if len(hip)== 1:
+        return hip
+    
+    # hip = [0,hip[-1]]+hip[1:-1]
+    hip.insert(1,hip.pop(-1))
+    idx = 1
+    son = idx * 2
+    while (len(hip)-1) > son:
+        if hip[son] > hip[son+1] and hip[son] > hip[idx]:
+            hip[idx] , hip[son] = hip[son] , hip[idx]
+            idx = son
+        elif hip[son+1] > hip[idx]:
+            hip[idx] , hip[son+1] = hip[son+1] , hip[idx]
+            idx = son + 1
+        else: # 자식이 둘다 본체보다 작을때
+            break
+        son = idx * 2
+
+    if (len(hip)-1) == son: #마지막에 남은 자식이 하나일경우 
+        if hip[son] > hip[idx]: # 비교후 교환
+            hip[idx] , hip[son] = hip[son] , hip[idx]
+
+    return hip
+
+n = int(input())
+hip = [0]
+for _ in range(n):
+    a = int(input())
+    if a:
+        hip = in_hip(hip,a)
+    else:
+        hip = out_hip(hip)

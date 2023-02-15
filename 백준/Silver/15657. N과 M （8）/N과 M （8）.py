2a307442cdbf21a3e8e068ece3b334c0d9563c80
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-02-15 14:35:03 +0900

    [Silver III] Title: N과 M (8), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+def nm(n,mm):
+    if mm == 0:
+        print(*visited)
+        return
+    for i in nums:
+        if mm == m:
+            visited.append(i)
+            nm(n,mm-1)
+            visited.pop()
+        elif len(visited) > 0 and visited[-1] <= i:
+            visited.append(i)
+            nm(n,mm-1)
+            visited.pop()
+    
+n, m = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+visited = []
+result = []
+nm(n,m)

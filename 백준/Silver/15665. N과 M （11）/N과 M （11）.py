22775faab19eeeb47a7214c5bcac9db973a0e4c7
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-02-16 09:07:29 +0900

    [Silver II] Title: N과 M (11), Time: 1408 ms, Memory: 66072 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,18 @@
+from collections import deque
+def nm(n,mm):
+    if mm == 0:
+        result_list = tuple([nums[j] for j in visited])
+        if result_list not in print_list:
+            print(*result_list)
+            print_list.add(result_list)
+        return
+    for i in range(len(nums)):
+        visited.append(i)   
+        nm(n,mm-1)
+        visited.pop()
+    
+n, m = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+visited = deque()
+print_list = set()
+nm(n,m)

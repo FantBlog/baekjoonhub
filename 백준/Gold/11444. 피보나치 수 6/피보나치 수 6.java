508f0fb71faa60061e04cb556eb16669a262cdef
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-03-14 23:21:06 +0900

    [Gold II] Title: 피보나치 수 6, Time: 84 ms, Memory: 11704 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long x = 1000000007L;
+    static HashMap<Long, Long> d = new HashMap<>();
+
+    public static long fibo(long n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 1;
+        } else if (d.get(n) != null) {
+            return d.get(n);
+        } else {
+            if (n % 2 == 1) {
+                long m = (n + 1) / 2;
+                long t1 = fibo(m);
+                long t2 = fibo(m - 1);
+                d.put(n, (t1 * t1 + t2 * t2) % x);
+                return d.get(n);
+            } else {
+                long m = n / 2;
+                long t1 = fibo(m - 1);
+                long t2 = fibo(m);
+                d.put(n, (2L * t1 + t2) * t2 % x);
+                return d.get(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long num = Long.parseLong(br.readLine());
+
+        bw.write(fibo(num) + "");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}

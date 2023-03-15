795a0b9380c0c67cea23bb0fcbacbaf2cc6256c7
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-03-16 00:17:46 +0900

    [Gold I] Title: 피보나치 수와 최대공약수, Time: 88 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Gold/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long x = 1000000007L;
+    static HashMap<Long, Long> d = new HashMap<>();
+
+    public static long fibo(long n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 1;
+        } else if (d.get(n) != null) {
+            return d.get(n);
+        } else {
+            if (n % 2 == 1) {
+                long m = (n + 1) / 2;
+                long t1 = fibo(m);
+                long t2 = fibo(m - 1);
+                d.put(n, (t1 * t1 + t2 * t2) % x);
+                return d.get(n);
+            } else {
+                long m = n / 2;
+                long t1 = fibo(m - 1);
+                long t2 = fibo(m);
+                d.put(n, (2L * t1 + t2) * t2 % x);
+                return d.get(n);
+            }
+        }
+    }
+
+    public static long gonyak(long N, long M) {
+        if (M == 0) return N;
+        return gonyak(M, N % M);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        bw.write(fibo(gonyak(N, M)) + "");
+        
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}

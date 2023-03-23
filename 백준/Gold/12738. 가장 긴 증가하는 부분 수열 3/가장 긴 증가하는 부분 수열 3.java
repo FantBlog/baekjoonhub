9d6b881032c2281cfa1c0f3369fa984ed54d6dd8
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-03-23 23:04:44 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 584 ms, Memory: 165744 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine()), point = 0, now, start, end, mid;
+        int[] arr = new int[N + 1];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i = 1; i <= N; i++) {
+        	now = Integer.parseInt(st.nextToken()) + 1000000001;
+        	
+        	if (arr[point] < now) {
+        		arr[++point] = now;
+        	}
+        	else {
+        		start = 1;
+                end = point;
+        		while (start != end) {
+        			mid = (start + end) / 2;
+        			if (arr[mid] < now) {
+        				start = mid + 1;
+        			}
+        			else {
+        				end = mid;
+        			}
+        		}
+        		arr[end] = now;
+        	}
+        }
+        bw.write(point + "");
+        bw.flush();
+    }
+}

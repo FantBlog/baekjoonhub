b9753b512a0ce0b4c87bbc463a1810b7b77d63f7
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-03-25 22:02:05 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1904 ms, Memory: 90572 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -82,7 +82,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        
-        init(607);
+        init(100003);
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()),M = Integer.parseInt(st.nextToken()), num;

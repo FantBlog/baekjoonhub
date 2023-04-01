ee725be57773314614a888a133653e0e69358bd9
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-04-02 02:18:38 +0900

    [Bronze IV] Title: R2, Time: 112 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt();
+        System.out.println(B * 2 - A);
+    }
+}

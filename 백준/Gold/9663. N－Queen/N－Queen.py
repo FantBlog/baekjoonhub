e1f6887c60e52889097e245d4efa467bf0132fd5
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-04-18 15:49:38 +0900

    [Gold IV] Title: N-Queen, Time: 12420 ms, Memory: 130292 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,142 +1,35 @@
-def push1D(x):
-    
-    pan1D[x] += 1
-    # 가로
-    # 몫이 같은애들 +1
-    dx = x + 1
-    while dx // N == x // N:
-        pan1D[dx] += 1
-        dx += 1
-        
-    dx = x - 1
-    while dx // N == x // N:
-        pan1D[dx] += 1
-        dx -= 1
+def findQueens(n):
+    arr = []
+    answer = 0
 
-    # 세로
-    # + n 만큼 + 1
+    for _ in range(n):
+        arr.append(0)
 
-    dx = x + N
-    while 0 <= dx < N*N:
-        pan1D[dx] += 1
-        dx += N
-        
-    dx = x - N
-    while 0 <= dx < N*N:
-        pan1D[dx] += 1
-        dx -= N
-    
-    # 대각선
-    # n+1,n-1 만큼 차이
-
-    dx = x + N + 1
-    while 0 <= dx < N*N:
-        if abs(dx//N - (dx-(N+1))//N) != 1:
-            break
-        pan1D[dx] += 1
-        dx += N + 1
-        
-    dx = x - (N + 1)
-    while 0 <= dx < N*N:
-        if abs(dx//N - (dx+(N+1))//N) != 1:
-            break
-        pan1D[dx] += 1
-        dx -= (N + 1)
-    
-    dx = x
-    while 0 <= dx+(N-1) < N*N:
-        if dx//N == (dx+(N-1))//N:
-            break
-        dx += (N-1)
-        pan1D[dx] += 1
-        
-    dx = x
-    while 0 <= dx-(N-1) < N*N:
-        if dx//N == (dx-(N-1))//N:
-            break
-        dx -= (N-1)
-        pan1D[dx] += 1
+    def f(deep, arr, n):
+        nonlocal answer
+        if (deep > n):
+            answer += 1
+            return
         
 
+        for index in range(n):
+            if (arr[index] > 0): continue
+            
+            x = False
+            for i in range(n):
+                if (arr[i] == 0): continue
+                if (abs(deep - arr[i]) == abs(index - i)):
+                    x = True
+                    break
 
-def pop1D(x):
-    pan1D[x] -= 1
-    # 가로
-    # 몫이 같은애들 +1
-    dx = x + 1
-    while dx // N == x // N:
-        pan1D[dx] -= 1
-        dx += 1
-        
-    dx = x - 1
-    while dx // N == x // N:
-        pan1D[dx] -= 1
-        dx -= 1
+            if x: continue
+            
+            arr[index] = deep
+            f(deep + 1, arr, n)
+            arr[index] = 0
 
-    # 세로
-    # + n 만큼 + 1
+    f(1, arr, n)
 
-    dx = x + N
-    while 0 <= dx < N*N:
-        pan1D[dx] -= 1
-        dx += N
-        
-    dx = x - N
-    while 0 <= dx < N*N:
-        pan1D[dx] -= 1
-        dx -= N
-    
-    # 대각선
-    # n+1,n-1 만큼 차이
+    return answer
 
-    dx = x + N + 1
-    while 0 <= dx < N*N:
-        if abs(dx//N - (dx-(N+1))//N) != 1:
-            break
-        pan1D[dx] -= 1
-        dx += N + 1
-        
-    dx = x - (N + 1)
-    while 0 <= dx < N*N:
-        if abs(dx//N - (dx+(N+1))//N) != 1:
-            break
-        pan1D[dx] -= 1
-        dx -= (N + 1)
-    
-    dx = x
-    while 0 <= dx+N-1 < N*N:
-        if dx//N == (dx+N-1)//N:
-            break
-        dx += N - 1
-        pan1D[dx] -= 1
-        
-    dx = x
-    while 0 <= dx-(N-1) < N*N:
-        if dx//N == (dx-(N-1))//N:
-            break
-        dx -= N - 1
-        pan1D[dx] -= 1
-
-def queen1D(i):
-    global count
-
-    if i == N:
-        count += 1
-        # print(result)
-        return
-    
-    for x in range(N*i, N*(i+1)):
-        if pan1D[x] == 0:
-            pnt.append(x)
-            push1D(x)
-            queen1D(i+1)
-            pnt.pop()
-            pop1D(x)
-
-
-N = int(input())
-pan1D = [0]*N*N
-pnt = []
-count = 0
-queen1D(0)
-print(count)
+print(findQueens(int(input())))

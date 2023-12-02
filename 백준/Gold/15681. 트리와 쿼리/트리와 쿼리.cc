59a13c0550b0c29a031d11253c3e4781ee247b2d
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-12-03 01:24:19 +0900

    [Gold V] Title: 트리와 쿼리, Time: 60 ms, Memory: 10980 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int LEN = 100'001;
+int visited[LEN];
+vector<int> g[LEN];
+
+int DFS(int index) {
+
+    for (int i = 0; i < g[index].size(); i++) {
+        int next = g[index][i];
+
+        if (visited[next] == 0) {
+            visited[next] = 1;
+            visited[index] += DFS(next);
+        }
+    }
+    return visited[index];
+}
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    int N, R, Q, U, V;
+
+    std::cin >> N >> R >> Q;
+
+    N--;
+    while (N--) {
+        std::cin >> U >> V;
+
+        g[U].push_back(V);
+        g[V].push_back(U);
+    }
+
+    visited[R] = 1;
+    DFS(R);
+
+    while (Q--) {
+        int query;
+        std::cin >> query;
+        std::cout << visited[query] << "\n";
+    }
+};

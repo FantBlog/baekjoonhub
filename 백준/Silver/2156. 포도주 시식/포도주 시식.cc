96a3314e9d3b5fbdc88654e733b962b56471a4f6
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-12-02 19:46:25 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -2,33 +2,25 @@
 
 using namespace std;
 
-const int LEN = 10'001;
-int input_list[LEN], DP[3][LEN];
-
-int max(int a, int b) {
-    if (a >= b) return a;
-    return b;
-}
-
-int max(int a, int b, int c) {
-    if (a >= b && a >= c) return a;
-    else if (b >= a && b >= c) return b;
-    return c;
+int m(int a, int b) {
+    return a > b ? a : b;
 }
 
 int main() {
-    int N, conti = 0;
-    std::cin >> N;
-    for (int i = 0; i < N; i++) std::cin >> input_list[i];
+    int N, n, b{}, d[4]{};
 
-    DP[1][0] = input_list[0];
+    std::cin >> N;
 
     for (int i = 0; i < N; i++) {
-        DP[0][i + 1] = max(DP[0][i], DP[1][i], DP[2][i]);
-        for (int j = 0; j < 2; j++) {
-            DP[j + 1][i + 1] = max(DP[j][i] + input_list[i + 1], DP[j + 1][i]);
-        }
-    }
+        std::cin >> n;
+        if (i == 0) d[3] = n;
+        else if (i == 1) d[3] = d[2] + n;
+        else d[3] = m(d[2], m(d[1] + n, d[0] + b + n));
 
-    std::cout << max(DP[0][N - 1], DP[1][N - 1], DP[2][N - 1]);
+        b = n;
+        d[0] = d[1];
+        d[1] = d[2];
+        d[2] = d[3];
+    }
+    std::cout << d[3];
 };

52e522169b8ca864a44d8b1141fd5da0ac4fb952
FantBlog <115213236+FantBlog@users.noreply.github.com>
2024-02-29 23:26:39 +0900

    [Gold V] Title: 행복 유치원, Time: 60 ms, Memory: 4556 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int LEN = 300000;
+int one[LEN], cha[LEN];
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    int n, k;
+    std::cin >> n >> k;
+
+    for (int i = 0; i < n;i++) std::cin >> one[i];
+
+    sort(one, one + n);
+
+    for (int i = 0; i < n - 1; i++) cha[i] = one[i + 1] - one[i];
+
+    sort(cha, cha + n - 1);
+
+    if (k >= n) {
+        std::cout << 0;
+    }
+    else {
+        long long result = 0;
+        for (int i = 0; i < n - k; i++) {
+            result += cha[i];
+        }
+        std::cout << result;
+    }
+};
